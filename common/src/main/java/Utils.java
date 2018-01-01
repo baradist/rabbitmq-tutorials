5a94662ac7b8ea0559f8159cb2e48f8abb909ec7
Oleg Grigoryev <oleg_grigoryev@epam.com>
2018-01-01 22:03:31 +0300

    common module extracted

---
+++ common/src/main/java/Utils.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Oleg Grigorjev on 31.12.2017.
+ */
+public class Utils {
+    static String getHost() {
+        try {
+            Properties properties = new Properties();
+            properties.load(Utils.class.getResourceAsStream("/app.properties"));
+            return properties.getProperty("rabbitmq.host");
+        } catch (IOException e) {
+            return "localhost";
+        }
+//        return PropertyResourceBundle.getBundle("app").getString("rabbitmq.host");
+    }
+}
